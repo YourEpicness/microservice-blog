79e7570226aed6fb35bd74c6165077a226c568c1
Julaam Diop <jeebe.diop@gmail.com>
2021-06-19 17:52:49 -0400

    added in query service to listen to any event posts, and refactored code to utilize query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+// import axios from "axios";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]); // empty array since API sends back array of comments
+const CommentList = ({ comments }) => {
+  // const [comments, setComments] = useState([]); // empty array since API sends back array of comments
+  //
+  // const fetchData = async () => {
+  //   const res = await axios.get(
+  //     `http://localhost:4002/posts/${postId}/comments`
+  //   ); // async retrieval of data from API
 
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    ); // async retrieval of data from API
+  //   setComments(res.data); //update state with fetched data
+  // };
 
-    setComments(res.data); //update state with fetched data
-  };
-
-  useEffect(() => {
-    fetchData(); // gets data on basically component mount
-  }, []); // empty array as second arg, so its only called once
+  // useEffect(() => {
+  //   fetchData(); // gets data on basically component mount
+  // }, []); // empty array as second arg, so its only called once
 
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>; // return a list element with a special key of our comment id
