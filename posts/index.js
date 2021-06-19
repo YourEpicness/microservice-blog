4842f00844d8cad3cab170d7fdc9efd647ae3b14
Julaam Diop <jeebe.diop@gmail.com>
2021-06-18 23:18:49 -0400

    added in simple event bus implementation, modified comments and posts to send events to bus

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const {randomBytes} = require('crypto');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
@@ -9,22 +10,40 @@ app.use(bodyParser.json());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
+// get any posts from /posts endpoint
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-
-app.post('/posts', (req, res) => {
-  const id = randomBytes(4).toString('hex'); // produces something like khjr3232
-  const {title} = req.body;
+// post any posts to the /posts endpoint
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex"); // produces something like khjr3232
+  const { title } = req.body;
 
   posts[id] = {
-    id, title
+    id,
+    title,
   };
+
+  // use axios to make post req
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
 
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+});
+
+// listening on port
 app.listen(4000, () => {
-  console.log('Listening on 4000');
+  console.log("Listening on 4000");
 });
