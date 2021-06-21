517ec2e96c68ee3315d3e4ab6b6fbb021d9d005a
Julaam Diop <jeebe.diop@gmail.com>
2021-06-21 18:00:29 -0400

    created ClusterIP config files for all the services and routed traffic using the clusterip instead of localhost

--- posts/index.js
+++ posts/index.js
@@ -26,7 +26,7 @@ app.post("/posts", async (req, res) => {
   };
 
   // use axios to make post req
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
